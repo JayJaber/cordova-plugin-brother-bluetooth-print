4beba41691680dcf23c34791590350978a13d0f9
JayJaber <calijayjaber@gmail.com>
2025-02-10 17:01:26 -0800

    updates

--- www/BrotherBluetoothPrint.js
+++ www/BrotherBluetoothPrint.js
@@ -1,56 +1,56 @@
 /*global cordova, module*/
 
-module.exports = {
-  greet: function (name, successCallback, errorCallback) {
-    cordova.exec(
-      successCallback,
-      errorCallback,
-      'BrotherBluetoothPrint',
-      'greet',
-      [name]
-    );
-  },
-  scan: function (successCallback, errorCallback) {
-    cordova.exec(
-      successCallback,
-      errorCallback,
-      'BrotherBluetoothPrint',
-      'scan'
-    );
-  },
-  print: function (macAddress, path, successCallback, errorCallback) {
-    cordova.exec(
-      successCallback,
-      errorCallback,
-      'BrotherBluetoothPrint',
-      'print',
-      [macAddress, path]
-    );
-  },
+// module.exports = {
+//   greet: function (name, successCallback, errorCallback) {
+//     cordova.exec(
+//       successCallback,
+//       errorCallback,
+//       'BrotherBluetoothPrint',
+//       'greet',
+//       [name]
+//     );
+//   },
+//   scan: function (successCallback, errorCallback) {
+//     cordova.exec(
+//       successCallback,
+//       errorCallback,
+//       'BrotherBluetoothPrint',
+//       'scan'
+//     );
+//   },
+//   print: function (macAddress, path, successCallback, errorCallback) {
+//     cordova.exec(
+//       successCallback,
+//       errorCallback,
+//       'BrotherBluetoothPrint',
+//       'print',
+//       [macAddress, path]
+//     );
+//   },
+// };
+
+var exec = require('cordova/exec');
+
+exports.greet = function (name, successCallback, errorCallback) {
+  cordova.exec(
+    successCallback,
+    errorCallback,
+    'BrotherBluetoothPrint',
+    'greet',
+    [name]
+  );
 };
 
-// var exec = require('cordova/exec');
+exports.scan = function (successCallback, errorCallback) {
+  cordova.exec(successCallback, errorCallback, 'BrotherBluetoothPrint', 'scan');
+};
 
-// exports.greet = function (name, successCallback, errorCallback) {
-//   cordova.exec(
-//     successCallback,
-//     errorCallback,
-//     'BrotherBluetoothPrint',
-//     'greet',
-//     [name]
-//   );
-// };
-
-// exports.scan = function (successCallback, errorCallback) {
-//   cordova.exec(successCallback, errorCallback, 'BrotherBluetoothPrint', 'scan');
-// };
-
-// exports.print = function (macAddress, path, successCallback, errorCallback) {
-//   cordova.exec(
-//     successCallback,
-//     errorCallback,
-//     'BrotherBluetoothPrint',
-//     'print',
-//     [macAddress, path]
-//   );
-// };
+exports.print = function (macAddress, path, successCallback, errorCallback) {
+  cordova.exec(
+    successCallback,
+    errorCallback,
+    'BrotherBluetoothPrint',
+    'print',
+    [macAddress, path]
+  );
+};
